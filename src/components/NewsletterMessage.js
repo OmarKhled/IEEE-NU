ab2baf43280ccc7be44d2a2b932ac4fe760772bf
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-07-18 15:14:40 +0200

    Newsletter pop implemented

--- src/components/NewsletterMessage.js
+++ src/components/NewsletterMessage.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
-const NewsletterMessage = () => {
+const NewsletterMessage = ({ remove }) => {
+    const turnOff = () => {
+        localStorage.setItem('revealPop', 'off')
+        remove()
+    }
+    const agree = () => {
+        alert('This section is still under development')
+        localStorage.setItem('revealPop', 'off')
+        remove()
+    }
     return (
         <div className="news-letter">
             <div className="newsletter-header">
                 <h3>Do you want to subscribe to our news letter ?</h3>
             </div>
             <div className="newsletter-body">
-                <button className="newsletter-btn">Yes</button>
-                <button className="newsletter-btn">Not really</button>
+                <button className="newsletter-btn" onClick={agree}>Yes</button>
+                <button className="newsletter-btn" onClick={turnOff}>Not really...</button>
             </div>
         </div>
     )
---
+++ src/components/NewsletterPop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewsletterMessage from './NewsletterMessage'
+import Character from './Character'
+
+const NewsletterPop = ({className, remove}) => {
+    return (
+        <div className={className}>
+            <Character />
+            <NewsletterMessage remove={remove}/>
+        </div>
+    )
+}
+
+export default NewsletterPop
