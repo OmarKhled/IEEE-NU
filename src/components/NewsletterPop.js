6cdcb82be9afc77d6c518ad1b90cb04d307b0e61
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-07-26 21:03:34 +0200

    Pop adjust

--- src/components/NewsletterPop.js
+++ src/components/NewsletterPop.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import NewsletterMessage from './NewsletterMessage'
-import Character from './Character'
+import React from "react";
+import NewsletterMessage from "./NewsletterMessage";
+import Character from "./Character";
 
-const NewsletterPop = ({className, remove}) => {
-    return (
-        <div className={className}>
-            <Character />
-            <NewsletterMessage remove={remove}/>
-        </div>
-    )
-}
+const NewsletterPop = ({ className, remove }) => {
+  return (
+    <div className={className}>
+      <Character />
+      <NewsletterMessage remove={remove} />
+    </div>
+  );
+};
 
-export default NewsletterPop
+export default NewsletterPop;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,44 +1,50 @@
-import React, { Fragment, useState, useEffect } from 'react'
-// Semantic UI 
-import 'semantic-ui-css/semantic.min.css';
+import React, { Fragment, useState, useEffect } from "react";
+// Semantic UI
+import "semantic-ui-css/semantic.min.css";
 // Components
-import Members from '../Members'
-import Carousel from '../Carousel'
-import NewsContainer from '../NewsContainer'
-import EventsContainer from '../EventsContainer';
-import PastParteners from '../PastParteners';
-import NewsletterPop from '../NewsletterPop';
+import Members from "../Members";
+import Carousel from "../Carousel";
+import NewsContainer from "../NewsContainer";
+import EventsContainer from "../EventsContainer";
+import PastParteners from "../PastParteners";
+import NewsletterPop from "../NewsletterPop";
 // CSS Stylesheet
-import '../../static/css/master.scss'
+import "../../static/css/master.scss";
 
-import { isMobile, mobileModel } from 'react-device-detect'
+import { isMobile, mobileModel } from "react-device-detect";
 
 const Home = () => {
-    const [reveal, setReveal] = useState('')
-    if (localStorage.getItem('revealPop') !== 'off') {
-        window.onscroll = function(ev) {
-            if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight && reveal == '') {
-                setTimeout(() => {
-                    setReveal('reveal')
-                }, 4000)
-            }
-        };
-    }
-    const remove = () => {
-        setReveal('')
-    }
-    return (
-        <Fragment>
-                <div className="px-5" style={{maxWidth: '65.125rem', margin: 'auto'}}>
-                    <Carousel />
-                    <NewsContainer/>
-                    <EventsContainer/>
-                    <Members />
-                    <PastParteners />
-                    <NewsletterPop className={`${reveal} ${isMobile? 'mobile' : ''}`} remove={remove} />
-                </div>
-        </Fragment>
-    )
-}
+  const [reveal, setReveal] = useState("");
+  if (localStorage.getItem("revealPop") !== "off") {
+    window.onscroll = function (ev) {
+      if (
+        window.innerHeight + window.scrollY >= document.body.scrollHeight &&
+        reveal == ""
+      ) {
+        setTimeout(() => {
+          setReveal("reveal");
+        }, 4000);
+      }
+    };
+  }
+  const remove = () => {
+    setReveal("");
+  };
+  return (
+    <Fragment>
+      <div className="px-5" style={{ maxWidth: "65.125rem", margin: "auto" }}>
+        <Carousel />
+        <NewsContainer />
+        <EventsContainer />
+        <Members />
+        <PastParteners />
+        <NewsletterPop
+          className={`${reveal} ${isMobile ? "mobile" : ""}`}
+          remove={remove}
+        />
+      </div>
+    </Fragment>
+  );
+};
 
-export default Home
+export default Home;
