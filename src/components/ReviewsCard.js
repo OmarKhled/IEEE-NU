92199b670e2185abef02bb8dab5083eb393449c6
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-08-02 11:27:19 +0200

    About content finished

--- src/components/ReviewsCard.js
+++ src/components/ReviewsCard.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Feed } from 'semantic-ui-react'
-const ReviewsCard = (props) => { 
-   
-    return <>
-    <Feed className="reviews ">
+import React from "react";
+import { Feed } from "semantic-ui-react";
+const ReviewsCard = (props) => {
+  return (
+    <>
+      <Feed className="reviews" style={{ height: "100%" }}>
         <Feed.Event className="card2 ">
-        <Feed.Label className="mx-auto">
-            <img src={props.review.image.default}/>
-        </Feed.Label>
-       
-            <hr style={{borderLeft: "1px solid #4f4f4f", height:"70%", margin: "auto 15px", display:"flex"}} />
-            <Feed.Content>
-                <Feed.Summary>
-                    {props.review.name}
-                </Feed.Summary>
-                <Feed.Extra text>
-                    <blockquote>
-                        "{props.review.review}"
-                    </blockquote>
-                </Feed.Extra>
-                
-            </Feed.Content>
+          <Feed.Label className="mx-auto">
+            <img src={props.review.image.default} />
+          </Feed.Label>
+          <hr
+            style={{
+              borderLeft: "1px solid #4f4f4f",
+              height: "70%",
+              margin: "auto 15px",
+              display: "flex",
+            }}
+          />
+          <Feed.Content>
+            <Feed.Summary>{props.review.name}</Feed.Summary>
+            <Feed.Extra text>
+              <blockquote>"{props.review.review}"</blockquote>
+            </Feed.Extra>
+          </Feed.Content>
         </Feed.Event>
-
-        </Feed>
-    
+      </Feed>
     </>
-}
+  );
+};
 
-export default ReviewsCard
+export default ReviewsCard;
