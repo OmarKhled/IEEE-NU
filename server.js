40deb0ebf0f6398d3cf59834a41f3d492a317609
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-08-04 12:16:29 +0200

    Adjustments

--- server.js
+++ server.js
@@ -67,9 +67,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/build"));
 
   const __dirname = path.resolve();
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    console.log("static");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 8000;
