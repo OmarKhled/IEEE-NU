804964ec5c7d6aabe6e13199434e0722e676277d
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2021-08-10 21:53:31 +0200

    Reviews adjust

--- client/src/components/ReviewsCards.js
+++ client/src/components/ReviewsCards.js
@@ -9,7 +9,7 @@ const Reviews = [
       "IEEE had a great role forming my personality, teaching me how to bare responsibilities and offered me some great Non-technical skills as planning events with world class companies, leadership skills and communication skills.",
   },
   {
-    image: require("../static/images/img/joe.jpg"),
+    image: require("../static/images/mohab.jpeg"),
     name: "Mohab Hafez",
     review:
       "started my journey in student activities in IEEE as an HR member in 2018. Then I joined IEEE’s high board as a secretary. Although it was so difficult to work during the pandemic, we managed to create a competition in which we learned how to win",
