3304fa9ab35bc1daba484d4b974416466f5fbcad
omar <o.abuelfetohahmed@nu.edu.eg>
2022-02-24 22:26:22 +0200

    Globe bigger on mobile devices

--- components/THREEJS/Globe.js
+++ components/THREEJS/Globe.js
@@ -17,7 +17,7 @@ const Globe = () => {
       globe.clientWidth < 446.875 && window.innerWidth >= 540
         ? globe.clientWidth / 446.875
         : globe.clientWidth < 446.875 && window.innerWidth < 400
-        ? (globe.clientWidth / 446.875) * 1.46
+        ? (globe.clientWidth / 446.875) * 1.3
         : 1
     );
     console.log(scale);
