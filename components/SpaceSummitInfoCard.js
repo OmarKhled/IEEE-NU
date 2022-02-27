6689ae0a517cc3aaea1212eb7a5153422b4c98c8
Sadeen-Alaa <69317732+Sadeen-Alaa@users.noreply.github.com>
2022-02-27 14:53:08 +0200

    Space summit info card

--- components/SpaceSummitInfoCard.js
+++ components/SpaceSummitInfoCard.js
@@ -4,7 +4,7 @@ export default function SpaceSummitInfoCard({ img, title, paragraph }) {
   return (
     <div className=" py-3 col-12 col-xs-6 col-md-4 col-lg-3">
       <div className="SpaceSummitInfoCard">
-        <svg class="svg">
+        <svg className="svg">
           <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox">
             <path d="M0.141,0 H0.859 a0.141,0.131,0,0,1,0.141,0.131 V0.664 S0.827,0.981,0.519,0.999 S0,0.803,0,0.803 V0.131 A0.141,0.131,0,0,1,0.141,0"></path>
           </clipPath>
