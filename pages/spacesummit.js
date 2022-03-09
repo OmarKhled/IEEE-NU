5c7488437c2f936dd0ea21098cccfd9ad9329384
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-03-09 18:59:13 +0200

    Build error resolve

--- pages/spacesummit.js
+++ pages/spacesummit.js
@@ -127,9 +127,9 @@ const Summit = () => {
               className="fb-video"
               src="https://www.youtube.com/embed/IVakM-cz4hc"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
 
