f32d58febeb8d2dc75efcbc21c2fd159dc13e016
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-03-12 21:50:49 +0200

    Entrepreneurship Bootcamp

--- client/src/components/pages/TEMSBootcamp.js
+++ client/src/components/pages/TEMSBootcamp.js
@@ -115,87 +115,90 @@ const TEMSBootcamp = () => {
   }, []);
 
   return (
-    <div className="showcase mt-3 m-auto" style={{ maxWidth: "50rem" }}>
-      <form
-        action="https://send.pageclip.co/EkGSRbgnXNZfSlDpgwXIMjhNzxA19ZPm/Entrepreneurship-Bootcamp"
-        method="post"
-        className="mt-2 pageclip-form"
-      >
-        <h5 className="mb-4">Entrepreneurship Bootcamp</h5>
-        {Object.keys(fields).map((key) => (
-          <div className="mb-3">
-            <h5 className="ms-2">
-              {/* {fields[key].type !== "range" && ( */}
-              <>
-                {fields[key].label}
-                {" : "}
-                {fields[key].required && (
-                  <span style={{ color: "red" }}>*</span>
-                )}
-              </>
-              {/* )} */}
-            </h5>
-            {fields[key].type === "select" ? (
-              <select
-                required={fields[key].required}
-                name={fields[key].name}
-                className="i-input mt-2"
-              >
-                {fields[key].options.map((option, index) => (
-                  <option key={index} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            ) : fields[key].type === "textarea" ? (
-              <textarea
-                className="mt-2"
-                style={{
-                  width: "100%",
-                  borderRadius: "7px",
-                  borderColor: "#a8a8a8",
-                  padding: "15px",
-                }}
-                required={fields[key].required}
-                type={fields[key].type}
-                name={fields[key].name}
-                placeholder={fields[key].placeholder}
-              />
-            ) : fields[key].type === "range" ? (
-              <div className="d-flex">
-                <input
-                  type="range"
+    <div className="px-5" style={{ minHeight: "90vh", margin: "auto" }}>
+      <div className="showcase mt-3 m-auto" style={{ maxWidth: "50rem" }}>
+        <form
+          action="https://send.pageclip.co/EkGSRbgnXNZfSlDpgwXIMjhNzxA19ZPm/Entrepreneurship-Bootcamp"
+          method="post"
+          className="mt-2 pageclip-form"
+        >
+          <h5 className="mb-4">Entrepreneurship Bootcamp</h5>
+          {Object.keys(fields).map((key) => (
+            <div className="mb-3">
+              <h5 className="ms-2">
+                {/* {fields[key].type !== "range" && ( */}
+                <>
+                  {fields[key].label}
+                  {" : "}
+                  {fields[key].required && (
+                    <span style={{ color: "red" }}>*</span>
+                  )}
+                </>
+                {/* )} */}
+              </h5>
+              {fields[key].type === "select" ? (
+                <select
                   required={fields[key].required}
+                  name={fields[key].name}
                   className="i-input mt-2"
+                >
+                  {fields[key].options.map((option, index) => (
+                    <option key={index} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              ) : fields[key].type === "textarea" ? (
+                <textarea
+                  className="mt-2"
+                  style={{
+                    width: "100%",
+                    borderRadius: "7px",
+                    borderColor: "#a8a8a8",
+                    padding: "15px",
+                  }}
+                  required={fields[key].required}
+                  type={fields[key].type}
                   name={fields[key].name}
                   placeholder={fields[key].placeholder}
-                  min={fields[key].min}
-                  defaultValue="1"
-                  max={fields[key].max}
-                  step="1"
-                  onChange={(e) =>
-                    (document.querySelector(`#${fields[key].name}`).innerHTML =
-                      e.target.value)
-                  }
                 />
-                <span id={fields[key].name}>1</span>
-              </div>
-            ) : (
-              <input
-                required={fields[key].required}
-                className="i-input mt-2"
-                type={fields[key].type}
-                name={fields[key].name}
-                placeholder={fields[key].placeholder}
-              />
-            )}
+              ) : fields[key].type === "range" ? (
+                <div className="d-flex">
+                  <input
+                    type="range"
+                    required={fields[key].required}
+                    className="i-input mt-2"
+                    name={fields[key].name}
+                    placeholder={fields[key].placeholder}
+                    min={fields[key].min}
+                    defaultValue="1"
+                    max={fields[key].max}
+                    step="1"
+                    onChange={(e) =>
+                      (document.querySelector(
+                        `#${fields[key].name}`
+                      ).innerHTML = e.target.value)
+                    }
+                  />
+                  <span id={fields[key].name}>1</span>
+                </div>
+              ) : (
+                <input
+                  required={fields[key].required}
+                  className="i-input mt-2"
+                  type={fields[key].type}
+                  name={fields[key].name}
+                  placeholder={fields[key].placeholder}
+                />
+              )}
+            </div>
+          ))}
+          <div className="d-flex justify-content-between mt-3">
+            <button className="btn-subscribe">Previous</button>
+            <input type="submit" className="btn-subscribe" />
           </div>
-        ))}
-        <div className="d-flex justify-content-between mt-3">
-          <button className="btn-subscribe">Previous</button>
-          <input type="submit" className="btn-subscribe" />
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
