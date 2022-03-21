06b81301e8323d2028d30a1046f359501c512b6a
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-03-21 14:13:43 +0200

    Cleaning models code

--- components/THREEJS/Globe.js
+++ components/THREEJS/Globe.js
@@ -3,27 +3,13 @@ import { useThree } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 
 const Globe = () => {
-  const state = useThree();
-
-  const globeMeshRef = useRef();
   const worldMap = useTexture("textures/125.png");
   const globe = document.querySelector(".globe .drei");
   const [scale, setScale] = useState(
     globe.clientWidth < 446.875 ? globe.clientWidth / 446.875 : 1
   );
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      state.setDpr(window.devicePixelRatio);
-      setScale(
-        globe.clientWidth < 446.875 && window.innerWidth >= 540
-          ? globe.clientWidth / 446.875
-          : globe.clientWidth < 446.875 && window.innerWidth < 400
-          ? (globe.clientWidth / 446.875) * 1.3
-          : 1
-      );
-      // console.log(scale);
-    });
+  const resize = () => {
     setScale(
       globe.clientWidth < 446.875 && window.innerWidth >= 540
         ? globe.clientWidth / 446.875
@@ -31,32 +17,11 @@ const Globe = () => {
         ? (globe.clientWidth / 446.875) * 1.3
         : 1
     );
-    state.setDpr(window.devicePixelRatio);
+  };
 
-    import("dat.gui").then((dat) => {
-      const gui = new dat.GUI();
-      gui.destroy();
-      if (globeMeshRef.current) {
-        gui
-          .add(globeMeshR.ef.current.rotation, "x")
-          .min(2 * -3.14159)
-          .max(2 * 3.14159)
-          .step(0.01)
-          .name("SphereXRotation");
-        gui
-          .add(globeMeshRef.current.rotation, "y")
-          .min(2 * -3.14159)
-          .max(2 * 3.14159)
-          .step(0.01)
-          .name("SphereYRotation");
-        gui
-          .add(globeMeshRef.current.rotation, "z")
-          .min(2 * -3.14159)
-          .max(2 * 3.14159)
-          .step(0.01)
-          .name("SphereZRotation");
-      }
-    });
+  useEffect(() => {
+    window.addEventListener("resize", resize);
+    resize();
   }, []);
 
   return (
@@ -65,8 +30,7 @@ const Globe = () => {
       <pointLight position={[537, 555, 0]} intensity={1} color={0x5ec8f6} />
       <mesh scale={scale} position={[0, 0, 0]}>
         <sphereGeometry args={[2.7, 100, 100]} />
-        <meshStandardMaterial map={worldMap} />
-        {/* <meshLambertMaterial color={"blue"}  map={worldMap} /> */}
+        <meshLambertMaterial map={worldMap} />
       </mesh>
     </>
   );
--- components/THREEJS/Logo.js
+++ components/THREEJS/Logo.js
@@ -1,95 +1,48 @@
-import { useState, useEffect, useRef } from "react";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { useLoader, useThree } from "@react-three/fiber";
+import { useState, useEffect } from "react";
+import { useGLTF } from "@react-three/drei";
 
 const Logo = () => {
-  const url = "/models/LOGO3.glb";
-  // const scale = 7.8;
-  const [model, setModel] = useState();
-
-  const loadedModel = useLoader(GLTFLoader, url, (loader) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath(
-      "https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/"
-    );
-    loader.setDRACOLoader(dracoLoader);
-  });
-
-  const state = useThree();
-  const logoMeshRef = useRef();
-
+  const scales = {
+    initialScale: 9,
+    mobileScale: 7,
+  };
   const logo = document.querySelector(".logo.hero .drei");
-  const initialScale = 9;
-  const mobileScale = 7;
-  const breakPoint = 450;
+
+  const { scene } = useGLTF("/models/LOGO3.glb");
   const [scale, setScale] = useState(
-    window.innerWidth < 830 ? mobileScale : initialScale
+    window.innerWidth < 830 ? scales.mobileScale : scales.initialScale
   );
   const [xPosition, setXPosition] = useState(logo.clientWidth < 440 ? 0 : 0);
 
-  useEffect(() => {
-    console.log(xPosition);
-    console.log(scale);
-  }, [xPosition, scale]);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setScale(window.innerWidth < 830 ? mobileScale : initialScale);
-      state.setDpr(window.devicePixelRatio);
-      setXPosition(logo.clientWidth < 440 ? (logo.clientWidth - 440) / 340 : 0);
-    });
-    state.setDpr(window.devicePixelRatio);
-    if (!model) {
-      setModel(loadedModel);
-    }
-
-    import("dat.gui").then((dat) => {
-      const gui = new dat.GUI();
-      gui.destroy();
-      gui
-        .add(logoMeshRef.current.rotation, "x")
-        .min(2 * -Math.PI)
-        .max(2 * Math.PI)
-        .step(0.01)
-        .name("XRotation");
-      gui
-        .add(logoMeshRef.current.rotation, "y")
-        .min(2 * -Math.PI)
-        .max(2 * Math.PI)
-        .step(0.01)
-        .name("YRotation");
-      gui
-        .add(logoMeshRef.current.rotation, "z")
-        .min(2 * -Math.PI)
-        .max(2 * Math.PI)
-        .step(0.01)
-        .name("ZRotation");
-    });
-  }, []);
-
-  if (model) {
-    return (
-      <>
-        <ambientLight intensity={0.4} />
-        <pointLight
-          castShadow
-          position={[3, 1, 3]}
-          intensity={1}
-          color={0x5ec8f6}
-        />
-        <mesh ref={logoMeshRef}>
-          <primitive
-            position={[xPosition, 0.2, 0]}
-            scale={[scale, scale, scale]}
-            object={model.scene}
-            dispose={null}
-          ></primitive>
-        </mesh>
-        {/* <axesHelper /> */}
-      </>
+  const resize = () => {
+    setScale(
+      window.innerWidth < 830 ? scales.mobileScale : scales.initialScale
     );
-  } else return null;
+    setXPosition(logo.clientWidth < 440 ? (logo.clientWidth - 440) / 340 : 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resize);
+  }, []);
+  return (
+    <>
+      <ambientLight intensity={0.4} />
+      <pointLight
+        castShadow
+        position={[3, 1, 3]}
+        intensity={1}
+        color={0x5ec8f6}
+      />
+      <mesh>
+        <primitive
+          position={[xPosition, 0.2, 0]}
+          scale={[scale, scale, scale]}
+          object={scene}
+          dispose={null}
+        ></primitive>
+      </mesh>
+    </>
+  );
 };
 
 export default Logo;
