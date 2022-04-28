0e5213667cf11ae01e73972476142b80733f16ac
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-04-28 21:15:21 +0200

    Adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -98,6 +98,18 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <span id="top"></span>
       <Navbar />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-B8YNHREF4V`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-B8YNHREF4V');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Footer />
     </>
