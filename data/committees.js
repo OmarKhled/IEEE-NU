fbf0c65f899b4a1599d02d81520b81043f9fd09d
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-06-27 20:42:08 +0200

    Committees Added :zap:

---
+++ data/committees.js
@@ -0,0 +1,41 @@
+// -	Social Media: to spread the branding identity of the branch, you need to a passionate team for graphic designers and editors to visualize the mission of the branch.
+
+export default [
+  {
+    mainImg: "/images/committees/technical.webp",
+    avatar: "/images/committees/icons/technical.webp",
+    title: "Technical",
+    children: `It is the backbone of the club, as it represents the vision and mission of it. The committee is responsible for Competitions, technical workshops, mega events about the latest technologies at the recent time.`,
+  },
+  {
+    mainImg: "/images/committees/pr.webp",
+    avatar: "/images/committees/icons/pr.webp",
+    title: "PR",
+    children: `It is responsible for creation, and coordination the deals between the club and other organization. Moreover, it supervises on the fundraising throughout events preparations.`,
+  },
+  {
+    mainImg: "/images/committees/marketing.webp",
+    avatar: "/images/committees/icons/marketing.webp",
+    title: "Marketing",
+    children: `The purpose of the Committee is providing focus on Sharing Exchange in general, as well as specific events. It assists staff in developing marketing strategies and initiatives incorporating the use of various media sources.`,
+  },
+  {
+    mainImg: "/images/committees/hr.webp",
+    avatar: "/images/committees/icons/hr.webp",
+    title: "HR",
+    children: `This committee is considered the main supervisor of the working flow in the other committees by calculating the working hours of each member and evaluating the performance.`,
+  },
+  {
+    mainImg: "/images/committees/operations.webp",
+    avatar: "/images/committees/icons/operations.webp",
+    title: "Operations",
+    children: `This committee is responsible for events management, and organizing, bringing required materials for other committees.`,
+  },
+  {
+    mainImg: "/images/committees/social.webp",
+    avatar: "/images/committees/icons/social.webp",
+    title: "Social Media",
+    children: `To spread the branding identity of the branch, you need to a passionate team for graphic designers and editors to visualize the mission of the branch.
+    `,
+  },
+];
--- pages/forms/recruitment.js
+++ pages/forms/recruitment.js
@@ -1,16 +1,39 @@
-import React from "react";
+import Head from "next/head.js";
+import committees from "../../data/committees.js";
+import InfoCard from "../../components/InforCard.js";
 
 const Recruitment = () => {
   return (
-    <div className="__root">
-      <main className="d-flex justify-content-center mx-4">
-        <section className="form">
-          <div className="d-flex justify-content-center align-items-center h-100">
-            <h5>This Form is closed for now, check again soon ...</h5>
-          </div>
-        </section>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Recruitment</title>
+        <meta property="og:title" content="Recruitment" key={"title"} />
+      </Head>
+      <div className="__root">
+        <main>
+          <section className="mt-4">
+            <h3 className="section-title">Branch Committies</h3>
+            <div className="row">
+              {committees.map((card) => (
+                <div key={card.title} className="col-12 col-sm-6 col-lg-4 mt-5">
+                  <InfoCard {...card} noCounter />
+                </div>
+              ))}
+            </div>
+          </section>
+          <section className="d-flex justify-content-center mx-4">
+            <div className="form contain">
+              <div className="body text-center ">
+                <h5 className="m-auto">
+                  The Recuitment Form is closed for now, check again later ...
+                </h5>
+                <p>Recruitment usually opens in October</p>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
   );
 };
 
