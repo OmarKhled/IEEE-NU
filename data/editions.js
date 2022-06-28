d7cbdbe6b11936fa502d7de25465463baf502a53
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-06-28 23:34:38 +0200

    Update Summit Speakers :fire:

--- data/editions.js
+++ data/editions.js
@@ -32,17 +32,22 @@ export default [
         description:
           "Eng. Ahmed Farid is a NASA-supported astronaut-scientist candidate in the POSSUM program and a member of the United Kingdom's Institution of Engineering Technology.",
       },
-      // {
-      //   name: "Mohamed El Koosy",
-      //   img: "/images/spacesummit/edition1/speakers/mohamedelkoosy.webp",
-      //   description:
-      //     "Mohamed Sallam is the only Qualified Egyptian / Arab Astronaut to go to mars with NASA on thier mars invasion program.",
-      // },
+      {
+        name: "Mohamed El Koosy",
+        img: "/images/spacesummit/edition1/speakers/mohamedelkoosy.webp",
+        description: "CEO and co-founder of The Egyptian Space Agency",
+      },
       {
         name: "Mohamed Tawfik",
         img: "/images/spacesummit/edition1/speakers/mohamedtawfik.webp",
         description:
           "A former Adjunct Professor and an assistant professor for aerospace engineering at Cairo University, also a former assistant professor at Emirates aviation college.",
+      },
+      {
+        name: "Hager Salah",
+        img: "/images/spacesummit/edition1/speakers/hagersalah.webp",
+        description:
+          "She is a researcher in the space weather monitoring center, with a bachelor of space science, and a master’s degree in astrophysics and space science. In addition, she is now a Ph.D. student in Astrophysics and space science. ",
       },
     ],
     winners: {
