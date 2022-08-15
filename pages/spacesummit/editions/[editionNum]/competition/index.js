98e2c11c2ccb4a0593506107dbe872d804148401
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-08-15 10:10:05 +0200

    REgistration form

--- pages/spacesummit/editions/[editionNum]/competition/index.js
+++ pages/spacesummit/editions/[editionNum]/competition/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useRouter } from "next/router";
 
 const Competition = () => {
-  const {
-    query: { editionNum },
-  } = useRouter();
-  const { form } = editions.find((elem) => elem.id == editionNum);
+  // const {
+  //   query: { editionNum },
+  // } = useRouter();
+  // const { form } = editions.find((elem) => elem.id == editionNum);
 
   return <main className="__root"></main>;
 };
