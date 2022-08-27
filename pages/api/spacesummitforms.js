2b3022ca5349cf3fe4b9af4fc9e208ca5a06e232
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-08-27 14:49:12 +0200

    Mails Automatic send

--- pages/api/spacesummitforms.js
+++ pages/api/spacesummitforms.js
@@ -1,7 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
+import nodemailer from "nodemailer";
+import QRCode from "qrcode";
+import SpaceSummitMail from "../../templates/SpaceSummitMail";
 import Pageclip from "pageclip";
+
 const pageclip = new Pageclip("api_enM7l7GWwLLzJvrkRfESsfSdte5N3Zrt");
+
 const firebaseConfig = {
   apiKey: "AIzaSyB7EedYToCVQMKEYFClGT-sRrx6o2DsJXM",
   authDomain: "ieee-nu.firebaseapp.com",
@@ -31,6 +36,45 @@ export default async function handler(req, res) {
           hadMeal: false,
         });
         console.log("Document written with ID: ", docRef.id);
+
+        const qr = await QRCode.toDataURL(docRef.id);
+
+        try {
+          console.log(process.env.EMAIL_PASSWORD);
+          let transporter = nodemailer.createTransport({
+            host: "smtp.office365.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: "ieeenu@nu.edu.eg",
+              pass: process.env.EMAIL_PASSWORD,
+            },
+          });
+
+          let info = await transporter.sendMail({
+            from: '"IEEE NU" <ieeenu@nu.edu.eg>', // sender address
+            to: data.email, // list of receivers
+            subject: "Egyptian Space Summit", // Subject line
+            attachments: [
+              {
+                filename: "QrCode.png",
+                content: qr.split(",")[1],
+                encoding: "base64",
+              },
+            ],
+            text: "Egyptian Space Summit Registration Confirmation", // plain text body
+            html: SpaceSummitMail(data.name),
+          });
+          console.log(`Message sent: ${data.email} ${info.messageId}`);
+          if (info) {
+            res.send({ success: true });
+            res.end();
+          }
+        } catch (error) {
+          console.log(error);
+          res.send({ msg: "stop screwing up you shit!" });
+          res.end();
+        }
 
         console.log(response.status, response.data, response.form);
         res.send({ success: response.status == 200 ? true : false });
