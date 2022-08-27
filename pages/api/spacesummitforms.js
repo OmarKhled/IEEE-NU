7beab6539aebd54f1b80ff6703a9811616b81241
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-08-27 13:12:53 +0200

    Firestore Back

--- pages/api/spacesummitforms.js
+++ pages/api/spacesummitforms.js
@@ -1,5 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 import Pageclip from "pageclip";
 const pageclip = new Pageclip("api_enM7l7GWwLLzJvrkRfESsfSdte5N3Zrt");
+const firebaseConfig = {
+  apiKey: "AIzaSyB7EedYToCVQMKEYFClGT-sRrx6o2DsJXM",
+  authDomain: "ieee-nu.firebaseapp.com",
+  projectId: "ieee-nu",
+  storageBucket: "ieee-nu.appspot.com",
+  messagingSenderId: "482944964315",
+  appId: "1:482944964315:web:7df6dc9efda403dc1a5efe",
+  measurementId: "G-MJFMV4NGHL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
@@ -9,6 +24,14 @@ export default async function handler(req, res) {
     pageclip
       .send(pageclipForm, req.body.data)
       .then(async (response) => {
+        const docRef = await addDoc(collection(db, "attendees"), {
+          ...req.body.data,
+          emailSent: false,
+          attended: false,
+          hadMeal: false,
+        });
+        console.log("Document written with ID: ", docRef.id);
+
         console.log(response.status, response.data, response.form);
         res.send({ success: response.status == 200 ? true : false });
         res.end();
