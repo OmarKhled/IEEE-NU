5dd1489493024cf2944b4365c5416ae66418f479
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-08-29 06:39:40 +0200

    Attendance form closed

--- pages/spacesummit/editions/[editionNum]/eventday/index.js
+++ pages/spacesummit/editions/[editionNum]/eventday/index.js
@@ -10,12 +10,14 @@ import Head from "next/head";
 const EventDay = () => {
   const { query } = useRouter();
   const [form, setForm] = useState(undefined);
+  const [edition, setEdition] = useState(undefined);
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (query !== undefined && query.editionNum) {
       const edition = editions.find((elem) => elem.id == query.editionNum);
+      setEdition(edition);
       setForm(edition.eventDayForm);
     }
   }, [query]);
@@ -45,87 +47,94 @@ const EventDay = () => {
       <Head>
         <title key={"title"}>Space Summit Registration Form</title>
       </Head>
-      {!success ? (
-        <form
-          style={{ maxWidth: "60rem", margin: "auto" }}
-          onSubmit={handleSubmit(submitAction)}
-          className="form mx-auto summitForm"
-          method="post"
-        >
-          <div className="body">
-            <h3 className="text-center mb-4">Space Summit Registration</h3>
-            {form?.map((field) => (
-              <div key={field?.label}>
-                <label style={{ fontWeight: "600" }}>
-                  {field?.label[0].toUpperCase() + field?.label.slice(1)}{" "}
-                  {field.required && <span style={{ color: "red" }}>*</span>}
-                </label>
-                {field.note && <small className="muted">{field.note}</small>}
-                {field.type !== "select" ? (
-                  <input
-                    type={field?.type}
-                    {...register(field?.label, {
-                      required: {
-                        value: field?.required,
-                        message: `${field?.label} Field is required`,
-                      },
-                      ...field?.validation,
-                    })}
-                    name={field?.label}
-                    placeholder={field?.placeholder}
-                    className={`${errors[field?.label] ? "err" : ""}`}
-                  />
-                ) : (
-                  <select
-                    {...register(field.label, {
-                      required: {
-                        value: field?.required,
-                        message: `${field.label} Field is required`,
-                      },
-                    })}
-                    name={field?.label}
-                    defaultValue={""}
-                    className={`${errors[field.label] ? "err" : ""}`}
-                  >
-                    {field.options?.map((option, index) => (
-                      <option
-                        value={index == 0 ? "" : option}
-                        disabled={index == 0}
-                        key={option}
-                      >
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                )}
-              </div>
-            ))}
-            {form && (
-              <Button
-                type={"submit"}
-                className="ms-auto mt-4 m-auto d-block"
-                disabled={loading && "disabled"}
-              >
-                Submit
-              </Button>
-            )}
+      {edition?.eventForm ? (
+        !success ? (
+          <form
+            style={{ maxWidth: "60rem", margin: "auto" }}
+            onSubmit={handleSubmit(submitAction)}
+            className="form mx-auto summitForm"
+            method="post"
+          >
+            <div className="body">
+              <h3 className="text-center mb-4">Space Summit Registration</h3>
+              {form?.map((field) => (
+                <div key={field?.label}>
+                  <label style={{ fontWeight: "600" }}>
+                    {field?.label[0].toUpperCase() + field?.label.slice(1)}{" "}
+                    {field.required && <span style={{ color: "red" }}>*</span>}
+                  </label>
+                  {field.note && <small className="muted">{field.note}</small>}
+                  {field.type !== "select" ? (
+                    <input
+                      type={field?.type}
+                      {...register(field?.label, {
+                        required: {
+                          value: field?.required,
+                          message: `${field?.label} Field is required`,
+                        },
+                        ...field?.validation,
+                      })}
+                      name={field?.label}
+                      placeholder={field?.placeholder}
+                      className={`${errors[field?.label] ? "err" : ""}`}
+                    />
+                  ) : (
+                    <select
+                      {...register(field.label, {
+                        required: {
+                          value: field?.required,
+                          message: `${field.label} Field is required`,
+                        },
+                      })}
+                      name={field?.label}
+                      defaultValue={""}
+                      className={`${errors[field.label] ? "err" : ""}`}
+                    >
+                      {field.options?.map((option, index) => (
+                        <option
+                          value={index == 0 ? "" : option}
+                          disabled={index == 0}
+                          key={option}
+                        >
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </div>
+              ))}
+              {form && (
+                <Button
+                  type={"submit"}
+                  className="ms-auto mt-4 m-auto d-block"
+                  disabled={loading && "disabled"}
+                >
+                  Submit
+                </Button>
+              )}
+            </div>
+          </form>
+        ) : (
+          <div className="form mx-auto summitForm d-flex justify-content-center align-items-center flex-column">
+            <p className="text-center">
+              Thanks for registering, your response has been recorded!
+            </p>
+            <small
+              style={{
+                textDecoration: "underline",
+                marginTop: "0.4rem",
+                cursor: "pointer",
+              }}
+              onClick={() => location.reload()}
+            >
+              Submit another response
+            </small>
           </div>
-        </form>
+        )
       ) : (
         <div className="form mx-auto summitForm d-flex justify-content-center align-items-center flex-column">
-          <p className="text-center">
-            Thanks for registering, your response has been recorded!
-          </p>
-          <small
-            style={{
-              textDecoration: "underline",
-              marginTop: "0.4rem",
-              cursor: "pointer",
-            }}
-            onClick={() => location.reload()}
-          >
-            Submit another response
-          </small>
+          <h3 className="text-center mb-2">Space Summit Registration</h3>
+          <p>This form is no longer accepting responses</p>
         </div>
       )}
     </main>
