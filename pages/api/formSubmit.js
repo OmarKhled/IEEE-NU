966656d10ecc006d010995510b2125cd6e702682
Omar Khaled <omarkhled.me@gmail.com>
2023-08-23 19:19:06 +0300

    Bioverse form open

--- pages/api/formSubmit.js
+++ pages/api/formSubmit.js
@@ -1,5 +1,6 @@
 import Pageclip from "pageclip";
-const pageclip = new Pageclip("api_enM7l7GWwLLzJvrkRfESsfSdte5N3Zrt");
+// const pageclip = new Pageclip("api_enM7l7GWwLLzJvrkRfESsfSdte5N3Zrt");
+const pageclip = new Pageclip("api_lUPpJXjlkLY3kbt6p65DdUMUrhMg3lKd");
 
 export default async function handler(req, res) {
   if (req.method == "POST") {
@@ -9,6 +10,7 @@ export default async function handler(req, res) {
     pageclip
       .send(pageclipForm, req.body.data)
       .then(async (response) => {
+        console.log(response);
         console.log(response.status, response.data, response.form);
         res.send({ success: response.status == 200 ? true : false });
         res.end();
